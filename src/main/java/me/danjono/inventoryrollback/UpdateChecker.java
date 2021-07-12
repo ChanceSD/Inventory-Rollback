ffa0cf73c9a89969612618220b5f7d20134338c1
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2021-07-12 03:45:29 +0100

    Fix update checker

--- src/main/java/me/danjono/inventoryrollback/UpdateChecker.java
+++ src/main/java/me/danjono/inventoryrollback/UpdateChecker.java
@@ -61,19 +61,35 @@ public class UpdateChecker {
         
         if (availableVersion.isEmpty()) {
         	result = UpdateResult.FAIL_SPIGOT;
-        	return;
-        } else if (availableVersion.equalsIgnoreCase(currentVersion)) {
+		} else if (isVersionAtLeast(currentVersion,availableVersion)) {
         	result = UpdateResult.NO_UPDATE;
-        	return;
-    	} else if (!availableVersion.equalsIgnoreCase(currentVersion)) {
+		} else {
     		result = UpdateResult.UPDATE_AVAILABLE;
-    		return;
     	}
-    	
-    	result = UpdateResult.FAIL_SPIGOT;
-    	
     }
    
+    public static final boolean isVersionAtLeast(final String v1, final String v2) {
+		if (v1.equals(v2))
+			return true;
+
+		final String[] v1Array = v1.split("\\.");
+		final String[] v2Array = v2.split("\\.");
+		final int length = Math.max(v2Array.length, v1Array.length);
+		try {
+			for (int i = 0; i < length; i++) {
+				final int x = i < v2Array.length ? Integer.parseInt(v2Array[i]) : 0;
+				final int y = i < v1Array.length ? Integer.parseInt(v1Array[i]) : 0;
+				if (y > x)
+					return true;
+				if (y < x)
+					return false;
+			}
+		} catch (final NumberFormatException ex) {
+			ex.printStackTrace();
+		}
+		return true;
+	}
+    
     public UpdateResult getResult() {
         return this.result;
     }
